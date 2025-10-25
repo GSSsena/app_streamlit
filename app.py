465f760c7fe190a2b6d43444ea0d0e9612bee2c2
Guilherme <guilhermedasilvasena960@gmail.com>
2025-10-25 10:59:56 -0300

    Projeto completo

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 import streamlit as st
 import requests
+import pandas as pd
 
-# st.set_page_config(page_title="Cidades do Brasil")
+st.set_page_config(page_title="Cidades do Brasil")
 
-# st.title(" algumas Cidades do Brasil")
-# st.write("Ola vou ,mostra algumas Cidaes do brasil para voce")
+st.title(" algumas Cidades do Brasil")
+st.write("Ola vou ,mostra algumas Cidaes do brasil para voce")
 
-# nome = st.text_input("coloque seu nome:")
+nome = st.text_input("coloque seu nome:")
 
-# if st.button("Enviar"):
-#     if nome:
-#         st.success(f"Olá, {nome} Seja bem-vindo")
-#     else:
-#         st.warning("Por favor, Não esqueça de coloca o nome antes de  clicar no botão.")
+if st.button("Enviar"):
+    if nome:
+        st.success(f"Olá, {nome} Seja bem-vindo")
+else:
+        st.warning("Por favor, Não esqueça de coloca o nome antes de  clicar no botão.")
 
 
 st.divider()
@@ -34,10 +35,12 @@ if resposta.status_code == 200:
         estado_nome = cidade_info["microrregiao"]["mesorregiao"]["UF"]["nome"]
         regiao_nome = cidade_info["microrregiao"]["mesorregiao"]["UF"]["regiao"]["nome"]
 
-        st.success(f"🏙️ Cidade selecionada: **{cidade_escolhida}**")
-        st.write(f"📍 Estado: {estado_nome}")
-        st.write(f"🌎 Região: {regiao_nome}")
-else:
-    st.error("Erro ao acessar a API do IBGE")
+        st.markdown(f"Cidade selecionada: **{cidade_escolhida}**")
+        st.markdown(f"Estado: {estado_nome}")
+        st.markdown(f"Região: {regiao_nome}")
+    else:
+        st.error("Erro ao acessar a API do IBGE") 
 
-commit()    
+    
+
+
